9bf681f92d32f5e14dfbdd31aee4e8d2a698b63a
Shayma Sharmeen <sshayma1612@gmail.com>
2026-09-06 04:40:23 +0600

    fix constraints

--- MediCart.Web/Models/AdminMedicineViewModel.cs
+++ MediCart.Web/Models/AdminMedicineViewModel.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MediCart.Web.Models
 {
-        public class DropdownOptionViewModel
+    public class DropdownOptionViewModel
     {
         public int Id { get; set; }
         public string Label { get; set; } = "";
@@ -46,10 +46,12 @@ namespace MediCart.Web.Models
 
         [StringLength(100)]
         public string? Manufacturer { get; set; }
-                [StringLength(150)]
+
+        [StringLength(150)]
         public string? GenericName { get; set; }
 
-        [StringLength(30)]
+        [Required(ErrorMessage = "Describe the unit and quantity (e.g. 1 strip of 10 tablets)")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Unit must be between 3 and 60 characters")]
         public string? Unit { get; set; }
 
         [StringLength(1000)]
