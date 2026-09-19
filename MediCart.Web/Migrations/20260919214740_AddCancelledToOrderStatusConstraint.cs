15d2b9b4a37d5a19e35577e01c5ce2bd9bc97a8f
Shayma Sharmeen <sshayma1612@gmail.com>
2026-09-20 03:58:24 +0600

    feat(orders-db): update CK_Order_Status check constraint to include Cancelled status

---
+++ MediCart.Web/Migrations/20260919214740_AddCancelledToOrderStatusConstraint.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediCart.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCancelledToOrderStatusConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Order_Status",
+                table: "Orders");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Order_Status",
+                table: "Orders",
+                sql: "\"Status\" IN ('Pending','Processing','Shipped','Delivered','Rejected','Cancelled')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Order_Status",
+                table: "Orders");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Order_Status",
+                table: "Orders",
+                sql: "\"Status\" IN ('Pending','Processing','Shipped','Delivered','Rejected')");
+        }
+    }
+}
